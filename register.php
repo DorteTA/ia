f6636ffab959848303d3597600408eca9aeaa10d
DorteTA <dorteta@gmail.com>
2015-06-01 14:14:11 +0200

    register

--- register.php
+++ register.php
@@ -1,52 +1,48 @@
 <?php
-/*---------------------------------
-index.php
-Start page with welcome
-The first page the visitor sees
----------------------------------*/
 
 include_once("inc/HTMLTemplate.php");
 include_once("inc/Connstring.php");
 
 $content = <<<END
 
-			
        	<div id="content">
 			<div class="row">
 				<div class="col-md-3">
 					<div class="panel panel-blue">
 						<div class="panel-heading">
-							<h3 class="panel-title">Nyheter</h3>
+							<h3 class="panel-title">Registrera dig</h3>
 						</div><!-- panel-heading -->
 						<div class="panel-body">
-							<div class="media">
-								<a class="pull-left" href="#">
-								<img class="media-object img-rounded" src="http://placehold.it/64x64" alt="...">
-								</a>
-								<div class="media-body">
-								<h4 class="media-heading">Se film om projektet Jämlik Ishall</h4>
-								<small>2014-04-30</small>
-								<p>"Förbundets utvecklingsprojekt "Jämlik Ishall" engagerar allt fler föreningar och kommuner runt om i landet.
-								 <p><a class="btn btn-blue btn-xs" role="button">Läs mer</a></p>
-								</div><!-- media body -->
-							</div><!-- media -->	
+							<form action="register.php" method="POST" class="blue" id="register-form">
+							
+								<label for="username">Användarnamn</label>
+								<input type="text" class="form-control" id="username" name="username" placeholder="Användarnamn">
+								
+								<label for="mail">E-post</label>
+								<input type="varchar" class="form-control" id="mail" name="mail" placeholder="din e-post @ adress">
+								
+								<label for="name">Namn</label>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Ditt förnamn">
+								
+								<label for="surname">Efternamn</label>
+								<input type="text" class="form-control" id="surname" name="surname" placeholder="Ditt efternamn">
+								
+								<label for="address">Adress</label>
+								<input type="text" class="form-control" id="adress" name="address" placeholder="Din post-adress">
+								
+								<label for="zipcode">Postnummer</label>
+								<input type="text" class="form-control" id="zipcode" name="zipcode" placeholder="Ditt postnummer">
+								
+								<label for="city">Ort</label>
+								<input type="text" class="form-control" id="city" name="cit" placeholder="Din ort">
+								
+								<label for="password">Lösenord</label>
+								<input type="password" class="form-control" id="password" name="password" placeholder="Ditt lösenord">
+								
+								<input class="btn btn-blue btn-xs" type="submit" value="Skapa konto" name="submit" />
+							</form>
 						</div><!-- panel-body -->
-						
-						<div class="panel-body">
-							<div class="media">
-								<a class="pull-left" href="#">
-								<img class="media-object img-rounded" src="http://placehold.it/64x64" alt="...">
-								</a>
-								<div class="media-body">
-								<h4 class="media-heading">Se film om projektet Jämlik Ishall</h4>
-								<small>2014-04-30</small>
-								<p>"Förbundets utvecklingsprojekt "Jämlik Ishall" engagerar allt fler föreningar och kommuner runt om i landet.
-								 <p><a class="btn btn-blue btn-xs" role="button">Läs mer</a></p>
-								</div><!-- media body -->
-							</div><!-- media -->	
-						</div><!-- panel-body -->
-						
-					</div><!-- panel panel-blue -->								
+					</div><!-- panel panel-blue -->						
 				</div><!-- col-md-3 -->
 				
 				<div class="col-xs-12 col-md-6">
@@ -89,7 +85,16 @@ $content = <<<END
 					
 							</div><!-- panel-body -->
 					</div><!-- panel panel-yellow -->
-			
+
+					<div class="panel panel-yellow">
+						<div class="panel-heading">
+							<h3 class="panel-title">Panel title</h3>
+						</div>
+						<div class="panel-body">
+						Panel content
+						</div><!-- panel body -->
+					</div><!-- panel yellow -->
+					
 				<!-- rad center, 2 kolumner layout-->
 				
 				<div class="col-xs-12 col-md-6 sans-padding-left pull-left">
@@ -131,31 +136,29 @@ $content = <<<END
 				</div><!-- col-xs-6 col-md-3 -->				
 			</div><!-- mitten -->	
 							
-				<!-- Rad högre -->
-				<div class="col-md-3 pull-right">
-					<div class="panel panel-blue">
-						<div class="panel-heading">
-							<h3 class="panel-title">Sponsorer</h3>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<p>På index-sidan ska här ligga en carousel m sponsorer och samarbetspartnare.
-							</p>
-							<p class="divider"></p>
-							<p>asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
-							sdf sdf asfa ssdfaj sdf. 
-							ksadfj asdf asdf asdf... asdf asdfasdf asdf sadf asdf as adfsfgsfg
-							sdfgsdfg dfg dfg df.
-							</p>
-						</div><!-- panel-body -->
-					</div><!-- panel panel-blue -->								
-				</div><!-- col-xs-6 col-md-3 -->
-			</div> <!-- row -->
-       </div><!-- AVsluta content DIV -->
-
+			<!-- Rad högre -->
+			<div class="col-md-3 pull-right">
+				<div class="panel panel-blue">
+					<div class="panel-heading">
+						<h3 class="panel-title">Sponsorer</h3>
+					</div><!-- panel-heading -->
+					<div class="panel-body">
+						<p>På index-sidan ska här ligga en carousel m sponsorer och samarbetspartnare.
+						</p>
+						<p class="divider"></p>
+						<p>asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
+						sdf sdf asfa ssdfaj sdf. 
+						ksadfj asdf asdf asdf... asdf asdfasdf asdf sadf asdf as adfsfgsfg
+						sdfgsdfg dfg dfg df.
+						</p>
+					</div><!-- panel-body -->
+				</div><!-- panel panel-blue -->								
+			</div><!-- col-xs-6 col-md-3 -->
+		</div> <!-- row -->
+    </div><!-- AVsluta content DIV -->
 END;
 
 echo $header;
 echo $content;
 echo $footer;
-
 ?>
