2c69880a0b602d9b22be80e49ac0a52f8782b579
Robjen12 <Robertjensen1985@hotmail.com>
2015-06-01 15:03:09 +0200

    ok

--- inc/Connstring.php
+++ inc/Connstring.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'Thunder14', 'konstakning'); // (HOST,  Användare, Lösenord, Databas)
+$mysqli = new mysqli('localhost', 'root', '', 'konstakning'); // (HOST,  Användare, Lösenord, Databas)
 
 if(mysqli_connect_error()) {
 	echo "Connect failed: " . mysqli_connect_error() . "<br>";
