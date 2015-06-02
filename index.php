6c3a05dbbc1dbe5c1e061f1ceb017e149d7e5e34
Robjen12 <Robertjensen1985@hotmail.com>
2015-06-02 14:22:29 +0200

    ok

--- index.php
+++ index.php
@@ -8,12 +8,40 @@ The first page the visitor sees
 include_once("inc/HTMLTemplate.php");
 include_once("inc/Connstring.php");
 
+$choosenarticlename = "";
+$choosenarticlemessage = "";
+
+if(!empty($_GET))
+{
+	$getartikelid = isset($_GET['ArtikelId']) ? $_GET['ArtikelId'] : '';
+
+	$query = <<<END
+
+		SELECT ArtikelId, ArtikelName, ArtikelMessage
+		FROM artikel
+		WHERE ArtikelId = "{$getartikelid}";
+END;
+
+	$res = $mysqli->query($query) or die();
+
+	if($res->num_rows == 1)
+	{
+		while($row = $res->fetch_object())
+		{
+			$choosenarticlename = $row->ArtikelName;
+			$choosenarticlemessage = $row->ArtikelMessage;
+			/*$choosenarticletimestamp = $row->ArtikelTimeStamp;*/
+		}
+	}
+}
+
 $artikelnews = "";
 
 $query = <<<END
 
 	SELECT *
 	FROM artikel
+	WHERE kategori = 'nyhet'
 	ORDER BY Artikeltimestamp
 END;
 
@@ -23,6 +51,7 @@ if($res->num_rows > 0)
 {
 	while($row = $res->fetch_object())
 	{
+		$artikelid = $row->ArtikelId;
 		$artikelname = $row->ArtikelName;
 		$artikelmessage = $row->ArtikelMessage;
 		$artikelsubtext = substr($artikelmessage, 0, 160);
@@ -37,9 +66,8 @@ if($res->num_rows > 0)
 								<img class="media-object img-rounded" src="http://placehold.it/64x64" alt="...">
 								</a>
 								<div class="media-body">
-								<h4 class="media-heading">{$artikelname}</h4>
+								<h4 class="media-heading"><a href="index.php?ArtikelId={$artikelid}">{$artikelname}</a></h4>
 								{$artikelsubtext}...<br>
-								 <p><a class="btn btn-blue btn-xs" role="button">Läs mer</a></p><br>
 								</div><!-- media body -->
 							</div><!-- media -->	
 						</div><!-- panel-body -->
@@ -66,83 +94,15 @@ $content = <<<END
 				<div class="col-xs-12 col-md-6">
 					<div class="panel panel-yellow">
 						<div class="panel-heading">
-							<h3 class="panel-title">Svenska landslaget</h3>
+							<h3 class="panel-title">{$choosenarticlename}</h3>
 						</div><!-- panel-heading -->
 						<div class="panel-body">
-							<p><img src="http://placehold.it/600x300" class="img-responsive img-rounded img-100x">					
-							Lucas ipsum dolor sit amet owen darth skywalker r2-d2 calamari jango darth calamari leia hutt.
-							Skywalker hutt grievous dagobah obi-wan yoda luke calamari antilles. Mustafar anakin kit fett
-							<strong>wookiee</strong> mon cade darth. Obi-wan kamino kessel naboo ponda organa mustafar.
-							Boba hutt solo ackbar. Wedge jinn skywalker mothma greedo antilles. Mustafar organa r2-d2
-							antilles moff antilles ponda darth.
-							</p>
-							<p>
-							Wicket anakin skywalker mara calamari kessel. Fisto jawa
-							cade c-3po naboo. Moff moff moff vader grievous c-3p0 organa.Lobot organa tusken raider kit
-							wedge mon bespin darth fett. Darth leia obi-wan organa. Mace sidious windu sidious cade moff
-							secura. R2-d2 wedge lobot jango jango ewok darth antilles anakin. Baba han jabba mara utapau.
-							Darth mandalore yoda darth qui-gon luke jango skywalker skywalker. Organa mace organa dantooine
-							ventress padmé.
-							</p>
+							<img src="http://placehold.it/600x300" class="img-responsive img-rounded img-100x">					
+							{$choosenarticlemessage}
 														
 						</div><!-- panel-body -->
 					</div><!-- panel panel-yellow -->
-					
-					<div class="panel panel-yellow">
-						<div class="panel-heading">
-							<h3 class="panel-title">Rubrik 2</h1>
-						</div><!-- panel-heading -->
-							<div class="panel-body">
-								<p>Lucas ipsum dolor sit amet wedge mace kessel skywalker palpatine ackbar skywalker mustafar
-								coruscant lobot. Darth yavin yoda obi-wan windu solo. Organa jinn dooku twi'lek solo amidala yoda
-								jade moff. Hutt fett yoda solo ventress k-3po hutt binks solo. Ahsoka bothan anakin owen. Leia amidala
-								skywalker organa luke jinn organa kit hutt. Yavin jinn lobot solo darth moff jabba ponda naboo. Boba moff
-								jawa solo padmé calamari. Wookiee dagobah jabba skywalker moff tatooine. Fett antilles sidious antilles
-								calrissian fisto naboo.
-								</p>
-					
-							</div><!-- panel-body -->
-					</div><!-- panel panel-yellow -->
-			
-				<!-- rad center, 2 kolumner layout-->
-				
-				<div class="col-xs-12 col-md-6 sans-padding-left pull-left">
-					<div class="panel panel-yellow">
-						<div class="panel-heading">
-							<h3 class="panel-title">Rubrik 3</h3>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<p>Ipsum lapsum tralalla lal
-							al laaa!
-							asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf asdf
-							as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg sdf
-							sdf asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf 
-							asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
-							</p>
-									
-						</div><!-- panel-body -->
-					</div><!-- panel panel-yellow -->
-				</div><!-- col-md-6 -->
-				
-				<!-- rad center, andra kolumn layout -->
-
-				<div class="col-md-6 sans-padding-right pull-left">
-					<div class="panel panel-yellow">
-						<div class="panel-heading">
-							<h3 class="panel-title">Rubrik 4</h3>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<p>Ipsum lapsum tralalla lal
-							al laaa!
-							asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf asdf
-							as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg sdf
-							sdf asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf 
-							asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
-							</p>
-									
-						</div><!-- panel-body -->
-					</div><!-- panel panel-yellow -->
-				</div><!-- col-xs-6 col-md-3 -->				
+								
 			</div><!-- mitten -->	
 							
 				<!-- Rad högre -->
