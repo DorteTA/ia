a0583221c9432c60e6c0b3460cd2b8fc0d5551d1
Robjen12 <Robertjensen1985@hotmail.com>
2015-06-02 11:50:49 +0200

    ok

--- search.php
+++ search.php
@@ -9,7 +9,9 @@ $search = "";
 
 if(isset($_GET['search']))
 {
-// Hämtar ut från guidereviewinfo där titlar liknar sökordet
+
+	$search = $_GET['search'];
+
 	$query = <<<END
 		SELECT * FROM artikel
 		WHERE artikelName LIKE '%{$search}%';
@@ -52,67 +54,12 @@ $content = <<<END
 				
 				<div class="col-xs-12 col-md-6">
 					{$article}
-
-					<div class="panel panel-yellow">
-						<div class="panel-heading">
-							<h3 class="panel-title">Rubrik 2</h1>
-						</div><!-- panel-heading -->
-							<div class="panel-body">
-								<p>Lucas ipsum dolor sit amet wedge mace kessel skywalker palpatine ackbar skywalker mustafar
-								coruscant lobot. Darth yavin yoda obi-wan windu solo. Organa jinn dooku twi'lek solo amidala yoda
-								jade moff. Hutt fett yoda solo ventress k-3po hutt binks solo. Ahsoka bothan anakin owen. Leia amidala
-								skywalker organa luke jinn organa kit hutt. Yavin jinn lobot solo darth moff jabba ponda naboo. Boba moff
-								jawa solo padmé calamari. Wookiee dagobah jabba skywalker moff tatooine. Fett antilles sidious antilles
-								calrissian fisto naboo.
-								</p>
-					
-							</div><!-- panel-body -->
-					</div><!-- panel panel-yellow -->
-			
-				<!-- rad center, 2 kolumner layout-->
 				
-				<div class="col-xs-12 col-md-6 sans-padding-left pull-left">
-					<div class="panel panel-yellow">
-						<div class="panel-heading">
-							<h3 class="panel-title">Rubrik 3</h3>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<p>Ipsum lapsum tralalla lal
-							al laaa!
-							asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf asdf
-							as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg sdf
-							sdf asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf 
-							asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
-							</p>
-									
-						</div><!-- panel-body -->
-					</div><!-- panel panel-yellow -->
-				</div><!-- col-md-6 -->
-				
-				<!-- rad center, andra kolumn layout -->
-
-				<div class="col-md-6 sans-padding-right pull-left">
-					<div class="panel panel-yellow">
-						<div class="panel-heading">
-							<h3 class="panel-title">Rubrik 4</h3>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<p>Ipsum lapsum tralalla lal
-							al laaa!
-							asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf asdf
-							as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg sdf
-							sdf asfa ssdfaj sdf ksadfj asdf asdf asdf asdf asdfasdf asdf sadf 
-							asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
-							</p>
-									
-						</div><!-- panel-body -->
-					</div><!-- panel panel-yellow -->
-				</div><!-- col-xs-6 col-md-3 -->				
-			</div><!-- mitten -->	
+				</div><!-- mitten -->	
 							
 				<!-- Rad högre -->
 				<div class="col-md-3 pull-right">
-					
+				</div>	
 			</div> <!-- row -->
        </div><!-- AVsluta content DIV -->
 
