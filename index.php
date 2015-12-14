0d3e32620d5ca8745516d5867adb8e887af5a450
DorteTA <dorteta@gmail.com>
2015-12-14 12:49:12 +0100

    stylat karusell gra

--- index.php
+++ index.php
@@ -221,7 +221,7 @@ $content = <<<END
 		<!-- Rad högre m sponsorkarusell-->
 
 		<div class="col-md-3 pull-right">
-			<div class="panel panel-blue">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h3 class="panel-title">Sponsorer</h3>
 				</div><!-- panel-heading -->
