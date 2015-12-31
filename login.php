be720e3390114d4d23de5df2af612e9a454d763b
DorteTA <dorteta@gmail.com>
2015-12-31 01:21:20 +0100

    fixat login

--- login.php
+++ login.php
@@ -11,7 +11,6 @@ include_once("inc/HTMLTemplate.php");
 $feedback = "";
 $content = "";
 $table = "";
-$editor ="";
 
 if(!empty($_POST)) {
 
@@ -31,7 +30,7 @@ if(!empty($_POST)) {
 	}
 	
 	if($username == '' || $password == '') {
-		$feedback = "<p class=\"text-yellow\">Fyll i alla uppgifter.</p>";
+		$feedback = '<p class=\"text-yellow\">Fyll i alla uppgifter.</p>';
 	}
 	else {
 	
@@ -65,120 +64,47 @@ END;
 			else {
 				$feedback = "<p class=\"text-yellow\">Fel lösenord eller användernamn.</p>";
 			}
+
+			//Hindrar XSS-attack
+			$username	= htmlspecialchars($username);
+			$password	= htmlspecialchars($password);
+
 			$res->close();
-		}		
+		}
+
 	}
 }
 
-
-//adm meny
-//Kollar att posterna inte är tomma
-
-// Variabler
-$username = "";
-$password = "";
-
-if(!empty($_POST)) {
-	include_once("inc/Connstring.php");
-	$editor ="admin";
-
-	$username =		isset($_POST['username']) ? $_POST['username'] : '';
-	$password =		isset($_POST['password']) ? $_POST['password'] : '';
-	$spamTest =		isset($_POST['address']) ? $_POST['address'] : '';
-
-	//Om fältet address INTE är tomt (som det bör vara) ges feedback om att man har upptäckt en spam-bot
-	if($spamTest != '') {
-		die("Jag tror du är en robot. Om jag har fel, gå tillbaka och prova igen.");
-	}
-	//Om fält m namn eller besked inte har fyllts i, får användaren en feedback att fylla i alla fält
-	if($username == '' || $password == '') {
-		$feedback = '<p class=\"text-yellow\">Var god fyll i alla fält.</p>';
-	
-	} else {
-		//SQL injections åtgärd
-		$username	= utf8_encode($mysqli->real_escape_string($username));
-		$password	= utf8_encode($mysqli->real_escape_string($password));
-	
-		date_default_timezone_set("Europe/Stockholm");
-	
-		//MySQL query som lägger in nya beskeder i databasen
-		$query = <<<END
-		--
-		-- Gets username and password based on user input
-		--
-		SELECT adminId, adminName, adminPassword
-		FROM {$editor}
-		WHERE adminName = "{$username}";
-		
-		
-END;
-
-		//Ger felmeddelande om databasen inte kan köras och hänvisar till felnummer, annars körs den
-		$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error);
-
-		if($res->num_rows == 1) {
-		$pswmd5 = md5($password);
-		$row = $res->fetch_object();
-		if($row->adminPassword == $pswmd5) {
-				session_start();
-				session_regenerate_id();
-				
-				$_SESSION["username"] = $username;
-				$_SESSION["userId"] = 	$row->adminId;
-				
-				header("Location:index.php");
-				
-			}
-			//Här kan olika feedback givas om användaren har skrivit fel användarnamn, men har valt att inte göra så, eftersom det ger evt hackare tips
-			//om att de har hittat rätt password eller anvädarenamn.
-			else {
-				$feedback = '<p class\"text-yellow\"Fel lösenord eller användarnamn.</p>';
-			}
-			$res->close();
-		}
-		else {
-			$feedback = '<p class\"text-yellow\"Fel lösenord eller användarnamn.</p>';
-		}
-		
-		$mysqli->close();
-	}
-
-}
-
-//Hindrar XSS-attack
-$username	= htmlspecialchars($username);
-$password	= htmlspecialchars($password);
-
+//Innehållet som visas via strängen $content
 $content = <<<END
-
 			
-       	<div id="content">
-			<div class="row">
-				<div class="col-md-3">
-					<div class="panel panel-blue">
-						<div class="panel-heading">
-							<h4 class="panel-title">Logga in</h4>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<form action="login.php" method="post" class="blue" id="login-form pull-right">
-							{$feedback}
-							<label for="username">Användarnamn</label>
-							<input type="text" class="form-control" id="username" name="username" value="" />
-							<label for="password">Lösenord</label>
-							<input type="password" class="form-control" id="password" name="password" value="" />
-							<input type="submit" class="btn btn-blue btn-xs" id="submit" value="logga in" />
-							<p><a href="register.php">Registrera dig</a></p>
-							</form>
-							
-						</div><!-- panel-body -->
-					</div><!-- panel panel-blue -->								
-				</div><!-- col-md-3 -->
-			</div><!-- row -->
-		</div><!-- content -->
+<div id="content">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="panel panel-blue">
 
+				<div class="panel-heading">
+					<h4 class="panel-title">Logga in</h4>
+				</div><!-- panel-heading -->
+
+				<div class="panel-body">
+					<form action="login.php" method="post" class="blue" id="login-form pull-right">
+						{$feedback}
+						<label for="username">Användarnamn</label>
+						<input type="text" class="form-control" id="username" name="username" value="" />
+						<label for="password">Lösenord</label>
+						<input type="password" class="form-control" id="password" name="password" value="" />
+						<input type="submit" class="btn btn-blue btn-xs" id="submit" value="logga in" />
+						<p><a href="register.php">Registrera dig</a></p>
+					</form>
+							
+				</div><!-- panel-body -->
+			</div><!-- panel panel-blue -->								
+		</div><!-- col-md-3 -->
+	</div><!-- row -->
+</div><!-- content -->
 
 END;
-
 
 echo $header;
 echo $content;
