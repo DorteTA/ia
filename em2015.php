87ffef836a374ea415ce57c33129332ef17a3645
DorteTA <dorteta@gmail.com>
2016-02-03 16:26:18 +0100

    enhetligt

--- em2015.php
+++ em2015.php
@@ -43,7 +43,7 @@ END;
 
 		<div class="panel panel-yellow">
 			<div class="panel-heading">
-				<h3 class="panel-title">{$artikelname}</h3>
+				<h3 class="panel-title blue bold">{$artikelname}</h3>
 			</div><!-- panel-heading -->
 				<div class="panel-body">
 					{$artikelmessage}<br><br>	
@@ -61,7 +61,7 @@ else
 {
 	$query = <<<END
 
-		SELECT ArtikelId, ArtikelName, ArtikelMessage, ArtikelTimeStamp, kategori
+		SELECT ArtikelId, ArtikelName, ArtikelMessage, ArtikelTimeStamp, Kategori
 		FROM artikel
 		WHERE kategori = 'em'
 		ORDER BY Artikeltimestamp
@@ -79,16 +79,17 @@ END;
 
 				$artikeltime = <<<END
 
-				<div class="panel panel-yellow">
-					<div class="panel-heading">
-						<h3 class="panel-title">{$artikelname}</h3>
-					</div><!-- panel-heading -->
-						<div class="panel-body">
-							{$artikelmessage}
-							{$artikeltimestamp}
-						</div><!-- panel-body -->
-				</div><!-- panel panel-yellow -->
-			
+<div class="panel panel-yellow">
+	<div class="panel-heading">
+		<h3 class="panel-title blue bold">{$artikelname}</h3>
+	</div><!-- panel heading -->
+
+	<div class="panel body">
+		{$artikelmessage}
+		{$artikeltimestamp}			
+	</div><!-- panel body -->
+
+</div><!-- panel panel yellow -->
 
 END;
 			}
@@ -97,7 +98,7 @@ END;
 // Hämtar ut undermenyn när användaren klickat på en länk
 $query = <<<END
 
-	SELECT ArtikelId, ArtikelName, ArtikelMessage, ArtikelTimeStamp, kategori
+	SELECT ArtikelId, ArtikelName, ArtikelMessage, ArtikelTimeStamp, Kategori
 	FROM artikel
 	WHERE kategori = 'em'
 	ORDER BY Artikeltimestamp
@@ -105,10 +106,10 @@ END;
 
 $res = $mysqli->query($query) or die();
 
-if($res->num_rows > 0)
-{
-	while($row = $res->fetch_object())
-	{
+if($res->num_rows > 0) {
+
+	while($row = $res->fetch_object()) {
+
 		$artikelId = $row->ArtikelId;
 		$artikelname = $row->ArtikelName;
 		$artikelmessage = $row->ArtikelMessage;
@@ -117,15 +118,15 @@ if($res->num_rows > 0)
 		$artikelnames .= <<<END
 
 
-			<div class="collapse-in" id="dokument">
+		<div class="collapse-in" id="dokument">
 								
-				<ul class="">
+			<ul class="meny">
           								
-	   				<a href="em2015.php?ArtikelId={$artikelId}">
-	   					<li>{$artikelname}</li>
-	   				</a>	   									
-	   			</ul>
-			</div><!-- collapse -->
+	   		<a href="em2015.php?ArtikelId={$artikelId}">
+	   			<li>{$artikelname}</li>
+	   		</a>	   									
+	   	</ul>
+		</div><!-- collapse -->
 					
 END;
 	}
@@ -133,50 +134,32 @@ END;
 $content = <<<END
 
 			
-       	<div id="content">
-			<div class="row">
-				<div class="col-md-3">
-					<div class="panel panel-blue">
+<div id="content">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="panel panel-yellow">
 
-						<div class="panel-heading">
-						
-						<!-- Om oss undermeny -->
-						
-							<h3 class="panel-title yellow">Tävla / EM2015</h3>
-						</div><!-- panel heading -->
-						
-					
-							
-						<div class="panel-body">
-							{$artikelnames}
-						</div><!-- panel body -->
-					</div><!-- panel panel blue -->									
-				</div><!-- col md 3 -->
+				<div class="panel-heading">						
+					<h3 class="panel-title blue bold">Tävla / EM2015</h3>
+				</div><!-- panel heading -->							
 				
-				<div class="col-xs-12 col-md-6">
-					{$artikeltime}	
-				</div><!-- mitten -->	
+				<div class="panel-body">
+					{$artikelnames}
+				</div><!-- panel body -->
+
+			</div><!-- panel panel blue -->									
+		</div><!-- col md 3 -->
+				
+		<div class="col-xs-12 col-md-6">
+			{$artikeltime}	
+		</div><!-- mitten -->	
 							
-				<!-- Rad högre -->
-				<div class="col-md-3 pull-right">
-					<div class="panel panel-blue">
-						<div class="panel-heading">
-							<h3 class="panel-title">Sponsorer</h3>
-						</div><!-- panel-heading -->
-						<div class="panel-body">
-							<p>På index-sidan ska här ligga en carousel m sponsorer och samarbetspartnare.
-							</p>
-							<p class="divider"></p>
-							<p>asdf as adfsfgsfgsdfgsdfg dfg dfg df gsdfg sdfg sdfg sdf gsdfg sdfg
-							sdf sdf asfa ssdfaj sdf. 
-							ksadfj asdf asdf asdf... asdf asdfasdf asdf sadf asdf as adfsfgsfg
-							sdfgsdfg dfg dfg df.
-							</p>
-						</div><!-- panel-body -->
-					</div><!-- panel panel-blue -->								
-				</div><!-- col-xs-6 col-md-3 -->
-			</div> <!-- row -->
-       </div><!-- AVsluta content DIV -->
+		<!-- Rad högre -->
+		{$sponsorer}
+		</div><!-- col-xs-6 col-md-3 -->
+
+	</div><!-- row -->
+</div><!-- content -->
 
 END;
 
